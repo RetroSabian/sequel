72d18eed76e588c5b6c736759ac4c564a92ecd06
RetroSabian <srajbully@retrorabbit.co.za>
2019-01-22 14:26:51 +0200

    createdatabase
    createuser
    added migrations

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [sabianrajbully] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.sabian.rajbully], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.sabian.rajbully]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.sabian.rajbully]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [sabianrajbully] FOR LOGIN [sabianrajbully]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.sabian.rajbully]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [sabianrajbully]
 GO
